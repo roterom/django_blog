46f7ab6a31a158382cf77639db0e5a00aec3f7a4
Rodrigo <roterom82@gmail.com>
2022-03-05 12:03:32 +0100

    Categories templates created

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import home
+from .views import *
 
 urlpatterns = [
     path('', home, name='index'),
+    path('generales/', generales, name='generales'),
+    path('programacion/', programacion, name='programacion'),
+    path('tecnologia/', tecnologia, name='tecnologia'),
+    path('videojuegos/', videojuegos, name='videojuegos'),
+    path('tutoriales/', tutoriales, name='tutoriales'),
+    
+    
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -4,3 +4,18 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def generales(request):
+    return render(request, 'generales.html')
+
+def programacion(request):
+    return render(request, 'programacion.html')
+
+def tutoriales(request):
+    return render(request, 'tutoriales.html')
+
+def tecnologia(request):
+    return render(request, 'tecnologia.html')
+
+def videojuegos(request):
+    return render(request, 'videojuegos.html')
