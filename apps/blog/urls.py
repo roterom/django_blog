b5aee91979350d900b453f24eb43dba4a720a7dc
Rodrigo <roterom82@gmail.com>
2022-04-07 19:10:30 +0200

    Added search bar

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -3,12 +3,12 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='index'),
-    path('<slug:slug>/', detallesPost, name='detalle_post'),    
     path('generales/', generales, name='generales'),
     path('programacion/', programacion, name='programacion'),
     path('tecnologia/', tecnologia, name='tecnologia'),
     path('videojuegos/', videojuegos, name='videojuegos'),
     path('tutoriales/', tutoriales, name='tutoriales'),
+    path('<slug:slug>/', detallesPost, name='detalle_post'),    
     
     
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,18 +1,29 @@
+from operator import ge
+from webbrowser import get
 from django.shortcuts import render
 from .models import Post, Categoria
+from django.shortcuts import get_object_or_404
+from django.db.models import Q
 # Create your views here.
 
 def home(request):
+    queryset = request.GET.get("buscar")
     posts = Post.objects.filter(estado = True)
+    if queryset:
+        posts = Post.objects.filter(Q(titulo__icontains = queryset) | Q(descripcion__icontains = queryset)).distinct()
     print (posts)
     return render(request, 'index.html', {'posts': posts})
 
 def detallesPost(request, slug):
-    post = Post.objects.get(slug=slug)
+    # post = Post.objects.get(slug=slug)
+    # Alternativa para hacer una validación de si ese post existe
+    post = get_object_or_404(Post, slug=slug)
     return render(request, 'post.html', {'detalle_post': post})
 
 def generales(request):
-    posts = Post.objects.filter(estado=True, categoria=Categoria.objects.get(nombre='General'))
+    # posts = Post.objects.filter(estado=True, categoria=Categoria.objects.get(nombre='General'))
+    #Alternativa para que busque por el nombre de la categoría sin que importe mayúsculas o minúsculas
+    posts = Post.objects.filter(estado=True, categoria=Categoria.objects.get(nombre__iexact='General'))
     return render(request, 'generales.html', {'posts': posts})
 
 def programacion(request):
